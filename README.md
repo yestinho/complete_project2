bbd01aa32e4a70bc5838db9f74045f7d729de65a
Yasin <ahmedyasinm62@gmail.com>
2018-12-14 13:32:09 -0800

    completed

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # complete_project2
+
+create database test1
+use test1
+
+CREATE TABLE `users` (
+	`id` INT(11) NOT NULL AUTO_INCREMENT,
+	`name` VARCHAR(100) NOT NULL,
+	`course` VARCHAR(100) NOT NULL,
+	`email` VARCHAR(100) NOT NULL,
+	PRIMARY KEY (`id`)
+)
+COLLATE='latin1_swedish_ci'
+ENGINE=InnoDB
+AUTO_INCREMENT=22
+;
---
+++ config.js
@@ -0,0 +1,15 @@
+var config = {
+	database: {
+		host:	  'localhost', 	// database host
+		user: 	  'root', 		// your database username
+		password: 'admin123', 		// your database password
+		port: 	  3309, 		// default MySQL port
+		db: 	  'test1' 		// your database name
+	},
+	server: {
+		host: '127.0.0.1',
+		port: '3000'
+	}
+}
+
+module.exports = config
